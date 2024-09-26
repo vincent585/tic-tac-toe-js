cb5c2fe2c3571ed66cd77cc91ffd72320ce20c93
Vincent Klimjack <vklimjack@gmail.com>
2024-09-26 09:07:31 +0100

    fix game winner logic

--- js/script.js
+++ js/script.js
@@ -1,29 +1,34 @@
 function createBoard() {
   let board = Array(3).fill(" ").map(() => Array(3).fill(" "));
   let winner = null;
-  const diagonals = [[board[0][0], board[1][1], board[2][2]], [board[0][2], board[1][1], board[2][0]]];
-  const rows = [board[0], board[1], board[2]];
-  const columns = [[board[0][0], board[1][0], board[2][0]], [board[0][1], board[1][1], board[2][1]], [board[0][2], board[1][2], board[2][2]]];
-  const winConditions = [diagonals, columns, rows];
+
+  const diagonals = () => [[board[0][0], board[1][1], board[2][2]], [board[0][2], board[1][1], board[2][0]]];
+  const rows = () => [board[0], board[1], board[2]];
+  const columns = () => [[board[0][0], board[1][0], board[2][0]], [board[0][1], board[1][1], board[2][1]], [board[0][2], board[1][2], board[2][2]]];
+  const winConditions = () => [...diagonals(), ...columns(), ...rows()];
 
   const checkForWinner = () => {
-    winConditions.forEach(direction => {
-      direction.forEach(condition => {
-        if (condition.every(square => square === 'X')) {
-          winner = 'X';
-          return;
-        }
-        else if (condition.every(square => square === 'O')) {
-          winner = 'O';
-          return;
-        }
-      });
-    });
+    for (const condition of winConditions()) {
+      if (condition.every(square => square === "X")) {
+        winner = "X";
+        break;
+      }
+      else if (condition.every(square => square === "O")) {
+        winner = "O";
+        break;
+      }
+    }
   };
+
+  const getWinner = () => winner;
 
   const updateSquare = (marker, square) => {
     const row = Math.floor((square / 3));
     col = square % 3;
+
+    if (board[row][col] != " ") {
+      alert("that square has already been played!");
+    }
 
     board[row][col] = marker;
   };
@@ -36,7 +41,7 @@ function createBoard() {
     }
   };
 
-  return { winner, checkForWinner, updateSquare, show };
+  return { getWinner, checkForWinner, updateSquare, show };
 }
 
 function createPlayer(marker) {
@@ -52,7 +57,7 @@ function createGame(board, playerArr) {
 
   const updateCurrentPlayer = () => currentPlayer = currentPlayer.playerMarker === 'X' ? players[1] : players[0];
   const playGame = () => {
-    while (gameBoard.winner === null) {
+    while (gameBoard.getWinner() === null) {
       gameBoard.show();
       let square = parseInt(prompt("pick a square to place your marker in (1-9): "));
       gameBoard.updateSquare(currentPlayer.playerMarker, square - 1);
@@ -60,7 +65,7 @@ function createGame(board, playerArr) {
       updateCurrentPlayer();
     }
     gameBoard.show();
-    alert(`Game over - ${gameBoard.winner}'s win!`);
+    alert(`Game over - ${gameBoard.getWinner()}'s win!`);
   }
 
   return { playGame };
