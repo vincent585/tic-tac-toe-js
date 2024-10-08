955ff22095a659cb77cdcaf8536c1fb5a435891b
Vincent Klimjack <vklimjack@gmail.com>
2024-10-08 11:24:44 +0100

    add form and logic for player setup

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-function createPlayer(marker) {
+function createPlayer(name, marker) {
   const getMarker = () => marker;
+  const getName = () => name;
 
-  return { getMarker };
+  return { getMarker, getName };
 }
 
 const board = (function () {
@@ -36,17 +37,27 @@ const board = (function () {
 })();
 
 const game = (function (board) {
-  const players = [createPlayer('X'), createPlayer('O')];
-  let currentPlayer = players[0];
+  let players = [];
+  let currentPlayer = null;
   let winner = null;
 
-  const getCurrentPlayer = () => currentPlayer.getMarker();
-  const updateCurrentPlayer = () => currentPlayer = currentPlayer.getMarker() === 'X' ? players[1] : players[0];
+  const getCurrentPlayer = () => currentPlayer;
+  const updateCurrentPlayer = (player = null) => {
+    if (player !== null) {
+      currentPlayer = player;
+    }
+    else {
+      currentPlayer = players.find(p => p !== currentPlayer);
+    }
+  }
+
+  const setPlayers = (playerArray) => players = playerArray;
 
   const playTurn = (square) => {
-    board.updateSquare(square, currentPlayer.getMarker());
+    board.updateSquare(square, currentPlayer);
 
     if (isGameOver()) {
+      // TODO - Change to display in DOM
       alert(`Game over - ${winner} wins!`);
       return;
     }
@@ -56,7 +67,7 @@ const game = (function (board) {
 
   const isGameOver = () => {
     if (hasWinner()) {
-      winner = currentPlayer.getMarker();
+      winner = currentPlayer.getName();
       return true;
     }
     else if (isTie()) {
@@ -86,11 +97,79 @@ const game = (function (board) {
     return false;
   };
 
-  return { playTurn, getCurrentPlayer }
+  return { playTurn, getCurrentPlayer, setPlayers, updateCurrentPlayer }
 })(board);
 
 const displayController = (function (game, board) {
   const gameboard = document.querySelector('.gameboard');
+  const setupModal = document.querySelector('dialog');
+  const form = document.querySelector('dialog form');
+  const startBtn = document.querySelector('#start');
+  const resetFormBtn = document.querySelector('#reset-form');
+  const playerOneMarker = document.getElementsByName('player-one-marker');
+  const playerTwoMarker = document.getElementsByName('player-two-marker');
+
+  startBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    let formData = new FormData(form);
+    if (!validateForm(formData)) {
+      return;
+    }
+
+    let players = [
+      createPlayer(formData.get("player-one-name"), formData.get("player-one-marker")),
+      createPlayer(formData.get("player-two-name"), formData.get("player-two-marker"))
+    ];
+
+    game.setPlayers(players);
+    game.updateCurrentPlayer(players.find(p => p.getMarker() === 'X'));
+    form.reset();
+    setupModal.close();
+    renderBoard();
+  });
+
+  resetFormBtn.addEventListener('click', () => {
+    form.reset();
+  });
+
+  const validateForm = (formToValidate) => {
+    const playerOneChecked = [...playerOneMarker].find(x => x.checked);
+    const playerTwoChecked = [...playerTwoMarker].find(x => x.checked);
+    const playerOneName = document.querySelector('#player-one-name');
+    const playerTwoName = document.querySelector('#player-two-name');
+
+    if (formToValidate.get("player-one-marker") === formToValidate.get("player-two-marker")) {
+      playerOneChecked.setCustomValidity("* Cannot have the same marker");
+      playerOneChecked.reportValidity();
+      playerTwoChecked.setCustomValidity("* Cannot have the same marker");
+      playerTwoChecked.reportValidity();
+
+      return false;
+    }
+
+    if (formToValidate.get("player-one-name").trim().length === 0) {
+
+      playerOneName.setCustomValidity("* Name cannot be empty");
+      playerOneName.reportValidity();
+
+      return false;
+    }
+
+    if (formToValidate.get("player-two-name").trim().length === 0) {
+      playerTwoName.setCustomValidity("* Name cannot be empty");
+      playerTwoName.reportValidity();
+
+      return false;
+    }
+
+    playerOneChecked.setCustomValidity("");
+    playerTwoChecked.setCustomValidity("");
+    playerOneName.setCustomValidity("");
+    playerTwoName.setCustomValidity("");
+
+    return true;
+  }
 
   const renderBoard = () => {
     const squares = board.getSquares();
@@ -116,14 +195,18 @@ const displayController = (function (game, board) {
       }
 
       let squareToUpdate = gameboard.querySelector(`button[data-index="${square}"]`);
-      squareToUpdate.innerText = player;
+      squareToUpdate.innerText = player.getMarker();
       game.playTurn(square);
     }
   };
 
+  const playerSetup = () => {
+    setupModal.showModal();
+  }
 
 
-  return { renderBoard };
+
+  return { renderBoard, playerSetup };
 })(game, board);
 
-displayController.renderBoard();
+displayController.playerSetup();
